689863b6cdf2b7567a5fae6f4d8bb25da9ff5262
sthhhh291 <sthhhh291@gmail.com>
2025-05-18 18:21:27 -0500

    minor changes to customers list

--- shop1/src/app/components/customers/CustomerList.tsx
+++ shop1/src/app/components/customers/CustomerList.tsx
@@ -1,12 +1,11 @@
 import { customers } from "@prisma/client";
-import Link from "next/link";
 import CustomerSearchCard from "./CustomerSearchCard";
 
 export default async function CustomerList(props: { customersList: customers[] }) {
     
     return (
     //   <div>
-        <div className="grid grid-cols-5 gap-4 items-center">
+        <div className="grid grid-cols-5 gap-4 justify-stretch">
           {props.customersList.map((cus:customers) => 
             <CustomerSearchCard key={cus.id} customer={cus} />
             )}
--- shop1/src/app/page.tsx
+++ shop1/src/app/page.tsx
@@ -3,12 +3,17 @@ import prisma from "@/lib/db";
 import CustomerList from "./components/customers/CustomerList";
 // import Link from "next/link";
 
-export default async function Home() {
-  const customersList  = await prisma.customers.findMany({take:20});
+export default async function Home(props:{},skip:number=0,take:number=40) {
+  const customersList  = await prisma.customers.findMany({take:take,skip:skip});
   // console.log(customersList);
   return (
     <div className="m-6 p-6">
+      <label htmlFor="customerSearch" className="text-bold p-2 m-2 ">Filter Customers</label>
+      <input className="rounded-md p-2 m-2 text-black" type="text" name="customerSearch" />
       <CustomerList customersList={customersList} />
+      <div id="pagination">
+        
+      </div>
     </div>
   );
 }
