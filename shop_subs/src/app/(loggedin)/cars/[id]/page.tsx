2d66ff0089a2653b8f0e3a43f0e9b92fafab95cd
sthhhh291 <sthhhh291@gmail.com>
2026-08-29 18:58:10 -0500

    got all types inserted. working on car/id page

--- shop_subs/src/app/(loggedin)/cars/[id]/page.tsx
+++ shop_subs/src/app/(loggedin)/cars/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getCarById, getCustomerById } from "@/app/actions";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import type { Customer, Car } from "@/types";
+import CustomerDetail from "../../ui/customer-detail";
 
 export default async function CustomerPage({
   params,
@@ -42,54 +43,17 @@ export default async function CustomerPage({
       <h2 className='text-xl font-bold bg-center align-center text-center p-4 rounded-lg shadow-md mt-4'>
         Car Details
       </h2>
-      <div className='grid grid-cols-4 gap-4 align-center text-center p-4 rounded-lg shadow-md mt-4'>
-        <div className='border border-gray-300 rounded p-4 mt-4'>
-          <p>
-            <strong>Name:</strong> {customer?.first_name} {customer?.last_name}
-          </p>
-          <p>
-            <strong>Notes:</strong> {customer?.notes}
-          </p>
-        </div>
-        {/* <div className='border border-gray-300 rounded p-4 mt-4'>
-          <h3 className='text-lg font-bold'>Phone Numbers</h3>
-          <ul>
-            {phones.map((phone) => (
-              <li key={phone.id}>
-                <strong>{phone.type}:</strong> {phone.number}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className='border border-gray-300 rounded p-4 mt-4'>
-          <h3 className='text-lg font-bold'>Email Addresses</h3>
-          <ul>
-            {emails.map((email) => (
-              <li key={email.id}>
-                <strong>{email.type}:</strong> {email.address}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className='border border-gray-300 rounded p-4 mt-4'>
-          <h3 className='text-lg font-bold'>Addresses</h3>
-          <ul>
-            {addresses.map((address) => (
-              <li key={address.id}>
-                {address.street}, {address.city}, {address.state}{" "}
-                {address.zip_code}
-              </li>
-            ))}
-          </ul>
-        </div> */}
-        {/* </div> */}
-        {/* <div className='gap-4 align-center text-center p-4 rounded-lg shadow-md mt-4'> */}
-        <h3 className='text-lg font-bold'>Car</h3>
+      <div className='grid grid-cols-2 gap-4 align-center text-center p-4 rounded-lg shadow-md mt-4'>
+        <CustomerDetail
+          customer={customer}
+          phones={[]}
+          emails={[]}
+          addresses={[]}
+        />
         <div>
-          {car.year} {car.make} {car.car_model}
+          {car.year} {car.make} {car.car_model} {car.engine && car.engine}{" "}
         </div>
       </div>
-      {/* </div> */}
     </>
   );
 }
--- shop_subs/src/types.ts
+++ shop_subs/src/types.ts
@@ -1,45 +1,148 @@
 export type Customer = {
-    id: number;
-    first_name: string;
-    last_name: string;
-    notes: string;
+  id: number;
+  first_name: string;
+  last_name: string;
+  notes: string;
 };
 
 export type Phone = {
-    id: number;
-    customer_id: number;
-    number: string;
-    type: string;
+  id: number;
+  customer_id: number;
+  number: string;
+  type: string;
 };
 
 export type Email = {
-    id: number;
-    customer_id: number;
-    address: string;
-    type: string;
+  id: number;
+  customer_id: number;
+  address: string;
+  type: string;
 };
 
 export type Address = {
-    id: number;
-    customer_id: number;
-    street: string;
-    city: string;
-    state: string;
-    zip_code: string;
+  id: number;
+  customer_id: number;
+  street: string;
+  city: string;
+  state: string;
+  zip_code: string;
 };
 
 export type Car = {
-    id: number;
-    customer_id: number;
-    year: number;
-    make: string;
-    car_model: string;
-    engine: string;
-    vin: string;
-    license: string;
-    fleet_number: string;
-    color: string;
-    notes: string;
+  id: number;
+  customer_id: number;
+  year: number;
+  make: string;
+  car_model: string;
+  engine: string;
+  vin: string;
+  license: string;
+  fleet_number: string;
+  color: string;
+  notes: string;
 };
 
-export type CarCustomer = Customer & Car
+export type CarCustomer = Customer & Car;
+
+export type Estimate = {
+  id: number;
+  car_id: number;
+  employee_id: number;
+  date: string;
+  hours: number;
+  mileage: number;
+  estimate_type: string;
+};
+
+export type Sub_estimate = {
+  id: number;
+  estimate_id: number;
+  description: string;
+};
+
+export type Labor = {
+  id: number;
+  sub_estimate_id: number;
+  description: string;
+  hours: number;
+  rate: number;
+  price: number;
+};
+
+export type Part = {
+  // works on oil too
+  id: number;
+  sub_estimate_id: number;
+  manufacturer: string;
+  part_number: string;
+  description: string;
+  quantity: number;
+  cost: number;
+  list: number;
+  price: number;
+};
+
+export type Totals = {
+  estimate_id: number;
+  sub_estimate_id: number;
+  employee_id: number;
+  date: string;
+  labor_total: number;
+  parts_total: number;
+  oil_total: number;
+  sub_total: number;
+  tax: number;
+  shop_fees: number;
+  grand_total: number;
+  cost_total: number;
+  parts_margin: number;
+  margin: number;
+};
+
+export type Income = {
+  id: number;
+  description: string;
+  payee: string;
+  date: string;
+  payment_type: string;
+  amount: 0;
+};
+
+export type PartsOrder = {
+  // id: number;
+  description: string;
+  payee: string;
+  date: string;
+  payment_type: string;
+  amount: number;
+};
+
+export type Employee = {
+  id: number;
+  customer_id: number;
+  first_name: string;
+  last_name: string;
+  title: string;
+  salary: number;
+};
+
+export type User = {
+  id: number;
+  username: string;
+  is_admin: boolean;
+  is_active: boolean;
+};
+
+export type Admin = {
+  id: number;
+  tax_rate: number;
+  labor_rate: number;
+  shop_fees_percent: number;
+  shop_fees_limit: number;
+};
+
+export type Markup = {
+  id: number;
+  amount: number;
+  markup_factor: number;
+};
